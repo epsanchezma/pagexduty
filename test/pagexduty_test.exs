6e6dac04145d0f314c0b3e8b1609d0c1fec784c7
Guillermo Iguaran <guilleiguaran@gmail.com>
2015-05-27 12:15:34 -0500

    Fix tabs

--- test/pagexduty_test.exs
+++ test/pagexduty_test.exs
@@ -9,13 +9,13 @@ defmodule PagexdutyTest do
 
   test "create_event creates a incident" do
 
-  	Pagexduty.Server.start_link("service_key")
+    Pagexduty.Server.start_link("service_key")
     response = %MockResponse{body: ~s({"status":"success","message":"Event processed","incident_key":"srv01/HTTP"})}
     with_mock HTTPoison, [post!: fn(_url, _body, _headers) -> response end] do
       response = Pagexduty.Server.trigger("My test incident", "srv01/HTTP", %{"detail" => "something"})
 
       assert response["status"] == "success"
       assert response["incident_key"] == "srv01/HTTP"
-   	 end
+    end
   end
 end
