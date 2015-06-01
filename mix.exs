901944e522159597e942662307dec0921ba6c2c9
Elba Sanchez Marquez <bita.sanchez2991@gmail.com>
2015-06-01 12:58:51 -0500

    Added description

--- mix.exs
+++ mix.exs
@@ -3,10 +3,12 @@ defmodule Pagexduty.Mixfile do
 
   def project do
     [app: :pagexduty,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.0",
+     description: description,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package,
      deps: deps]
   end
 
@@ -27,9 +29,23 @@ defmodule Pagexduty.Mixfile do
   #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
   #
   # Type `mix help deps` for more examples and options
+
+  defp description do
+    """
+    A Pagerduty client for Elixir.
+    """
+  end
+
   defp deps do
     [{:httpoison, "~> 0.6"},
      {:exjsx, "~> 3.1.0", app: false},
      {:mock, "~> 0.1.1", only: :test}]
+  end
+
+  defp package do
+    [ files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      contributors: ["Elba Sanchez, Guillermo Iguaran"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/ride/pagexduty"} ]
   end
 end
